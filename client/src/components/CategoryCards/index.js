8e7afc465a37cc228a81e54386898b5119cb05d7
mascarelloa <amfm0793@gmail.com>
2021-05-28 11:34:01 -0400

    adding image files and getting them to append form within the project.

--- client/src/components/CategoryCards/index.js
+++ client/src/components/CategoryCards/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import "./style.css"
+import math from "../../3.png"
+import arts from "../../4.png"
+import geography from "../../2.png"
+import history from "../../5.png"
+import science from "../../1.png"
+import sports from "../../6.png"
+
 
 const CategoryCards = () => {
   return (
@@ -8,33 +15,33 @@ const CategoryCards = () => {
             <h2>Get Started!</h2>
         <div className="cat-container">
             <div className="cat-card">
-                <img src="https://tinyurl.com/33fb2ewa" id="math"/>
+                <img src={math} id="math"/>
                 <h1>Math</h1>
             </div>
 
 
             <div className="cat-card">
-                <img src="https://tinyurl.com/2p6y5w72" id="entertainment"/>
+                <img src={arts} id="entertainment"/>
                 <h1>Arts</h1>
             </div>
 
             <div className="cat-card">
-                <img src="https://tinyurl.com/7tmazuvt" id="geography"/>
+                <img src={geography} id="geography"/>
                 <h1>Geography</h1>
             </div>
 
             <div className="cat-card">
-                <img src="https://tinyurl.com/9f43hbwa" id="history"/>
+                <img src={history} id="history"/>
                 <h1>History</h1>
             </div>
 
             <div className="cat-card">
-                <img src="https://tinyurl.com/3pwvjtbp" id="science"/>
+                <img src={science} id="science"/>
                 <h1>Science</h1>
             </div>
 
             <div className="cat-card">
-                <img src="https://tinyurl.com/kys77p4e" id="sports"/>
+                <img src={sports} id="sports"/>
                 <h1>Sports</h1>
             </div>
         </div>
