d0156202ed188ee8dc3a9704251aa7e0b94dc418
Kevin Eddie <keddie024@gmail.com>
2021-06-01 15:52:22 -0400

    updated quiz creation form. state working, but choices still needs work

--- client/src/components/NewQuizForm/index.js
+++ client/src/components/NewQuizForm/index.js
@@ -16,20 +16,32 @@ const NewQuizForm = (onCreate) => {
     const [visibility, setPublic] = useState(true);
     const handleVisChange = e => {
         setPublic(e.target.value);
-        console.log(visibility);
     }
 
-    const [questions, setQuestions] = useState([{
+    const blankQuestion = {
         question: '',
-        choices: [],
+        choices: [,,,,],
         answer: ''
-    }]);
-
-    const addQuestion = e => {
-        setQuestions(questions => [...questions, e])
-        // console.log(questions);
     }
 
+    const [questions, setQuestions] = useState([
+        { ...blankQuestion }
+    ]);
+
+    const handleAddQuestion = () => {
+        setQuestions([...questions, { ...blankQuestion }]);
+        console.log(questions);
+    }
+
+    const selectChoice = e => {
+        document.getElementById(e.target.name).value = document.getElementById(e.target.value).value;
+    }
+
+    const handleInputChange = e => {
+        const updatedQuestions = [...questions];
+        updatedQuestions[e.target.dataset.idx][e.target.className] = e.target.value;
+        setQuestions(updatedQuestions);
+    }
 
 
     return (
@@ -60,47 +72,51 @@ const NewQuizForm = (onCreate) => {
                 <label htmlFor="public">Public</label><br />
                 <input id="private" type="radio" name="type" value="false" onClick={handleVisChange} />
                 <label htmlFor="private">Private</label><br />
-                <div id="questionSection">
-                    <button onClick={addQuestion}>Add Question</button>
+                <button onClick={handleAddQuestion}>Add Question</button>
 
-                    {
-                        questions.map((value, index) => {
-                            let question = `question-${index}`;
-                            let choice = questions[index].choices;
-                            let answer = `answer-${index}`;
-                            // console.log(questions);
-                            // console.log(questions[0].choices);
+                {
+                    questions.map((value, index) => {
+                        let question = `question-${index}`;
+                        let choice = `choice-${index}`;
+                        let answer = `answer-${index}`;
 
-                            return (
-                                <div key={index}>
-                                    <label htmlFor={question}>{`Question #${index + 1}`}</label>
-                                    <input type="text" name={question} placeholder="Question Here" />
+                        return (
+                            <div key={index} id={index}>
+                                <label htmlFor={question}>{`Question #${index + 1}`}</label>
+                                <input className="question" type="text" name="question" data-idx={index} placeholder="Question Here" value={questions[index].question} onChange={handleInputChange} />
 
-                                    {/* // ToDo: Find a solution to loop over choices. Once working, will convert to additional component. */}
+                                {/* ToDo: Find a solution to get the choices into the state. Current code does not work (possibly the syntax of the value on line 92?) Once working, will convert to additional component. */}
 
-                                    <div>
-                                        <label>{`Choice #1`}</label>
-                                        <input type="text" name={choice} placeholder="Choice Here" />
+                                <div>
+                                    <label>Choice #1: </label>
+                                    <input className="choices" type="text" placeholder="Choice Here" data-idx={index} value={questions[index].choices[0]} onChange={handleInputChange}/>
+                                    <input id={choice + 'abox'} type="radio" name={answer} data-idx={index} value={`${choice}a`} onClick={selectChoice} />
+                                    <br />
 
-                                        <label>{`Choice #2`}</label>
-                                        <input type="text" name={choice} placeholder="Choice Here" />
+                                    <label>Choice #2: </label>
+                                    <input className="choices" type="text" placeholder="Choice Here" data-idx={index} value={questions[index].choices[1]} />
+                                    <input id={choice + 'bbox'} type="radio" name={answer} data-idx={index} value={`${choice}b`} onClick={selectChoice} />
+                                    <br />
 
-                                        <label>{`Choice #3`}</label>
-                                        <input type="text" name={choice} placeholder="Choice Here" />
+                                    <label>Choice #3: </label>
+                                    <input className="choices" type="text" placeholder="Choice Here" data-idx={index} value={questions[index].choices[2]} />
+                                    <input id={choice + 'cbox'} type="radio" name={answer} data-idx={index} value={`${choice}c`} onClick={selectChoice} />
+                                    <br />
 
-                                        <label>{`Choice #4`}</label>
-                                        <input type="text" name={choice} placeholder="Choice Here" />
-                                    </div>
+                                    <label>Choice #4: </label>
+                                    <input className="choices" type="text" placeholder="Choice Here" data-idx={index} value={questions[index].choices[3]} />
+                                    <input id={choice + 'dbox'} type="radio" name={answer} data-idx={index} value={`${choice}d`} onClick={selectChoice} />
 
-
-                                    <label htmlFor={answer}>Answer</label>
-                                    <input type="text" name={answer} placeholder="Answer Here" />
                                 </div>
-                            )
-                        })
-                    }
 
-                </div>
+
+                                <label htmlFor={answer}>Answer: </label>
+                                <input className="answer" type="text" name={answer} data-idx={index} value={questions[index].answer} onChange={handleInputChange} />
+                            </div>
+                        )
+                    })
+                }
+
                 <button className="btn btn btn-primary" type='submit'>Submit</button>
             </div>
         </form>
