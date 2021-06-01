7966c558840be345b914ed6f96622283e5ea873a
mascarelloa <amfm0793@gmail.com>
2021-06-01 18:10:31 -0400

    styling login/sign up forms with images.

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,4 +1,6 @@
 import React, { useRef } from 'react'
+import './LoginForm.css'
+import knowsy2 from "../../knowsy2.png"
 
 function LoginForm({ onLogin }) {
 
@@ -17,12 +19,16 @@ function LoginForm({ onLogin }) {
 					});
 				}}
 			>
-				<div className="form-group">
-					<input className="form-control" ref={userNameRef} type='text' name="username" placeholder='Enter Username' /><br />
-					<input className="form-control" ref={passwordRef} type='password' name="password" placeholder='Password' /><br />
-					<button className="btn btn-success" type='submit'>
+				<div className="login-content">
+					<img src={knowsy2}/>
+				<div className="form-group"><h1>Welcome back!</h1>
+					<input className="form-control" ref={userNameRef} type='text' name="username" placeholder='Enter Username' /><div className="input-title"><p>Username</p></div><br />
+					
+					<input className="form-control" ref={passwordRef} type='password' name="password" placeholder='Password' /><div className="input-title"><p>Password</p></div><br />
+					<button className="login" type='submit'>
 						Submit
 						</button>
+				</div>
 				</div>
 			</form>
 	)
---
+++ client/src/pages/AllQuizzes.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import QuizCard from "../components/QuizCard";
+import "../components/QuizCard/QuizCard.css";
+import API from "../utils/API";
+
+const AllQuizzes = () => {
+  const [quizzes, setQuizzes] = useState([]);
+  useEffect(() => {
+    loadQuizzes();
+  }, []);
+
+  function loadQuizzes() {
+    API.filterQuizzesPublic()
+      .then((res) => setQuizzes(res.data))
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div>
+      <QuizCard quizzes={quizzes} />
+    </div>
+  );
+};
+
+export default AllQuizzes;
