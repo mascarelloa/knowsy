ec85d727d54ee5188830abbd8757e1c61106d95a
Kevin Eddie <keddie024@gmail.com>
2021-06-02 14:44:32 -0400

    quizzes can now be posted to the database from the form

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -2,12 +2,13 @@ import React from 'react';
 import NewQuizForm from '../components/NewQuizForm';
 
 const Create = () => {
+
     return (
         <div>
-            <h1>This is rendered on the Create page.</h1>
-           <NewQuizForm />
+            <NewQuizForm  />
         </div>
     )
+
 }
 
 export default Create;
